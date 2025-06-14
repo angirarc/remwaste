2dc5abe879700c04f145dd34f2b9ba6c32cf0dfd
Reinhardt Angira <reinhardtcollins@live.com>
2025-06-14 13:15:56 +0300

    fix: properly handling errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,15 @@
 export const determineErrorMessage = (e: unknown) => {
     let message = 'Unknown error occurred';
-    if (isFetchError(e)) {
-        if (e.response) {
-            message = e.response.data.message;
-        } else if (e.request) {
-          message = 'No response received';
+    if (e instanceof Error) {
+        if ('response' in e) {
+            const responseError = e as { response?: { data?: { message?: string } } };
+            message = responseError.response?.data?.message ?? 'API error occurred';
+        } else if ('request' in e) {
+            message = 'No response received';
+        } else {
+            message = e.message;
         }
     }
-
+    
     return message;
 }
